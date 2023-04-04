9d107e0b1bfb6fd3b8ed2c670016f256a30875cc
nhtbao101 <nhtbao101@gmail.com>
2023-04-04 14:14:47 +0700

    test commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Chat
         </a>
       </header>
     </div>
